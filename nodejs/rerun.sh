284f26c9254c80648d6d776f3ab58e43aa78dd5b
Brenden Vogt <bvogtdev@gmail.com>
2019-07-20 04:39:52 -0700

    added nodejs adjustments for smaller image size

--- nodejs/rerun.sh
+++ nodejs/rerun.sh
@@ -1,16 +1,4 @@
-# NodeJS Docker Example
-
-## Build 
-```
 docker build -t testnode .
-```
-
-## Remove old container
-```
 docker rm -f testnode
-```
-
-## Run
-```
 docker run -d -p 8081:8080 --name testnode testnode
-```
+
