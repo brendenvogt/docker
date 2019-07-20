aa179d5adafbe872a46c8e04049acbae02f33294
Brenden Vogt <bvogtdev@gmail.com>
2019-07-20 04:14:29 -0700

    updated working python example

--- python/main.py
+++ python/main.py
@@ -1,11 +1,13 @@
 # flask_web/app.py
 
-From flask import Flask
+from flask import Flask
+# from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+def hello_world():
+    return 'Hey, we have Flask in a Docker container! Cool!'
 
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
